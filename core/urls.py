cabab4640c1c7ef90e64fa0b7d4dd3d7e0f82d80
DavisWere <devisodhis10@gmail.com>
2024-03-12 11:44:41 +0300

    debug

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('upload/', views.upload_image, name='upload_image'),
-    path('upload/success/', views.upload_success, name='upload_success'),
+    path('upload/success/', views.upload_image, name='upload_success'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, redirect
+
 from .forms import SkinDiseaseImageForm
+from django.contrib import messages
 
 def upload_image(request):
+    success_message = None  # Initialize success message variable
+    error_message = None  # Initialize error message variable
+
     if request.method == 'POST':
         form = SkinDiseaseImageForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('upload_success')
+            if request.FILES.get('image', False):  # Check if an image file is uploaded
+                form.save()
+                success_message = 'Image uploaded successfully'
+                messages.success(request, success_message)
+                return render(request, 'upload_image.html', {'form': form, 'success_message': success_message})
+            else:
+                error_message = 'Please select an image to upload'
+        else:
+            error_message = 'Form is not valid. Please correct the errors.'
+
     else:
         form = SkinDiseaseImageForm()
-    return render(request, 'upload_image.html', {'form': form})
 
-def upload_success(request):
-    return render(request, 'upload_success.html')
+    return render(request, 'upload_image.html', {'form': form, 'success_message': success_message, 'error_message': error_message})
